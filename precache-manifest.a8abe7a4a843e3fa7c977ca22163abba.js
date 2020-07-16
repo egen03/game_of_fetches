78e6f2fab7d200ca35f8cb26dae81c69dc81f316
Student_GA111 <egen03@gmail.com>
2020-07-16 11:48:00 -0400

    Updates

---
+++ precache-manifest.a8abe7a4a843e3fa7c977ca22163abba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6782ac8528968ed7038653b0d71b20ce",
+    "url": "/game_of_fetches/index.html"
+  },
+  {
+    "revision": "08dacff75a7b59b099bf",
+    "url": "/game_of_fetches/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2518237f83aa42c02636",
+    "url": "/game_of_fetches/static/js/2.17b8aaee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game_of_fetches/static/js/2.17b8aaee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08dacff75a7b59b099bf",
+    "url": "/game_of_fetches/static/js/main.15378cde.chunk.js"
+  },
+  {
+    "revision": "6d9a6b455fa96063c1af",
+    "url": "/game_of_fetches/static/js/runtime-main.205d8662.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/game_of_fetches/static/media/logo.5d5d9eef.svg"
+  }
+]);
